42b7f7a685fda8b8732d308325303621de8d7ad5
vivekmatta <49398002+vivekmatta@users.noreply.github.com>
2019-10-18 18:06:05 -0400

    Add files via upload

--- IsPrime.java
+++ IsPrime.java
@@ -1,5 +1,16 @@
 public class IsPrime {
 
+    // Finds all prime numbers less than 100.
+    public static void main(String [] args){
+        for (int i = 2; i < 100; i++){
+            boolean b = isPrime(i);
+            if (b == true){
+                System.out.println(i);
+            }
+        }
+    }
+
+    // Function that checks through every number from 0 - 99 and sees if it is a prime number or not.
     public static boolean isPrime(int x){
         int count = 0;
         for (int i = 2; i < x; i++){
@@ -16,14 +27,7 @@ public class IsPrime {
 
 
 
-    public static void main(String [] args){
-        for (int i = 2; i < 100; i++){
-            boolean b = isPrime(i);
-            if (b == true){
-                System.out.println(i);
-            }
-        }
-    }
+
 
 
 }
